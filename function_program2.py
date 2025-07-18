53c6708ae7fa2ba56e55a0463d0769a96391fdc1
Dhrisya <dhrisya2005@gmail.com>
2025-07-18 13:46:06 +0530

    functions day 3

--- function_program2.py
+++ function_program2.py
@@ -26,6 +26,8 @@ greetings(age=20,name="ashwin")"""
 c,b=sample(3,6)
 print(c,b)"""
 
+
+# global keyward
 """c=5
 def sample():
     global c
@@ -39,9 +41,49 @@ print(c)
     fun()    
 fun()"""
 
-def fact(num):
+
+# recurssion
+"""def fact(num):
     if num==1:
         return num
     else:
         return num*fact(num-1)
-print(fact(20))
+print(fact(5))"""
+
+
+# reverse recurssion
+"""def Rev(s):
+    if len(s)==0:
+        return s
+    else:
+        return Rev(s[1:])+s[0]
+print(Rev("ashwin"))"""
+
+
+"""odds=[k for k in range(1,18) if k%2==1]
+print(odds)
+evens=[k for k in range(1,17) if k%2==0]
+print(evens)"""
+
+#lambda
+"""v=lambda a,b=0:a+b
+print(v(5))"""
+
+"""is_even=lambda num:True if num%2==0 else False
+print(is_even(7))
+"""
+"""is_even=lambda num:f"{num} is even" if num%2==0 else f"{num} is odd"
+print(is_even(3))"""
+
+
+"""is_even=lambda num:f"{num} is even" if num%2==0 else f"{num} is odd"
+a=int(input("enter your number :"))
+print(is_even(a))"""
+
+"""def squares(num):
+    return num**2
+number=[1,3,4,7]
+print(set(map(squares,number)))"""
+
+"""numbers=[1,3,4,5]
+print(list(map(lambda x:x**2, numbers)))"""
