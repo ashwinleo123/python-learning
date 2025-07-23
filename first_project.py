c0546af2b25ae3deb27eac22b58f2f47c9792202
Dhrisya <dhrisya2005@gmail.com>
2025-07-23 14:01:07 +0530

    todo project

--- first_project.py
+++ first_project.py
@@ -1,12 +1,19 @@
+tasks=[]
 while True:
- print("==========TO DO LIST========== \n 1.add task \n 2.show task \n 3.mark task as done \n 4.exit")
+ print("==========TO DO LIST========= \n 1.add task \n 2.show task \n 3.mark task as done \n 4.exit")
  num=int(input("enter your choice:"))
  if num==1:
-  num2=int(input("how many task you want to add:"))
-  print(num2)
-  num3=int(input("enter the task:"))
-  print("task added")
-  print(num3)
+  num=int(input("how many task you want to add:"))
+  print(num)
+  tasks.append(num)
+  for x in range(num+1):
+    a=input("enter the task:")
+    print(a)
+    print("Task added")
+ if num==2:
+  print()
+
+     
   
   
 
